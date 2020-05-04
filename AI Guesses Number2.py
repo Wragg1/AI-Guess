2ff84574ee0ed94e3819ed658b79fcca7e3d3205
Wragg1 <33753600+Wragg1@users.noreply.github.com>
2020-05-04 13:21:23 +0100

    Update AI Guesses Number2.py
    Change:
    Ensure user input number is between 1-100
    import random now occurs once.

--- AI Guesses Number2.py
+++ AI Guesses Number2.py
@@ -1,17 +1,20 @@
+import random
 print("Number Guessing Game")
 print("Do you want to pick a random number for the computer to guess? Yes/No")
 randomchoice = input()
 if randomchoice.lower() == 'yes': #converts to one case, captures Yes, YES, YEs...
+#if randomchoice.startswith('y'): #would accept Y.. Ye.. Yes...
     print("A random number will be selected to guess")
-    import random
     number = random.randint(1,100)
     print("Your random number is", number)
-else: #anything other than yes
-    print("Please select a number")
-    number = input()
-    print("You have entered", number)
+else: #anything other than yes (includes "y")
+    while True:
+        number = int(input("Please enter a number between 1 and 100: "))
+        if 1 <= number <= 100:
+            break
+        print('Your number was not between 1 and 100, please try again')
+    print("You have selected", number)
 print("The computer will now guess your number")
-import random
 aiguess = random.randint(1,100)
 number = int(number)
 guesses = 0
@@ -32,3 +35,4 @@ while aiguess != number:
         aiguess = random.randint(lowguess,highguess)
 print("The computer guessed the correct number with",aiguess)
 print("It took the computer", guesses+1, "guesses to find your number")
+input()
